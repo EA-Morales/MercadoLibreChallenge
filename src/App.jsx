0981eb3edf400ce4ebdcbb8b0e566cbdad995f35
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-08-05 20:36:43 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import "./styles/App.css";
+import '@/styles/App.css';
+
+import product from '@/Mockup/product.json';
 
 function App() {
-	return <h1 className='text-5xl'>hola mundo</h1>;
+	return <h1 className='text-5xl text-green-600'>{product.title}</h1>;
 }
 
 export default App;
